2abac5aa01700a69275c475b831ea134b7d54859
Hengry <hengry@jubo.health>
2022-08-15 17:19:48 +0800

    stop

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,26 @@ const calculate = (values: number[], calculateIndices: number[]) => values
   .reduce(
     (accu, v, i) => calculateFunctions[calculateIndices[i]].func(accu, v),
     values[0],
-  );
+);
+
+// const calculate = (values: number[], calculateIndices: number[]) => {
+//   let process = [...values];
+//   calculateIndices.forEach((calcIndex, index) => {
+//     if (calcIndex > 1) {
+//       const calcFunc = calculateFunctions[calcIndex].func;
+//       const result = calcFunc(process[index], process[index + 1]);
+//       process = process
+//         .slice(0, index)
+//         .concat(result)
+//         .concat(process.slice(index + 1));
+//     }
+//   });
+//   return process.slice(1, values.length)
+//     .reduce(
+//       (accu, v, i) => calculateFunctions[calculateIndices[i]].func(accu, v),
+//       values[0],
+// );
+// };
 
 const indexToCalulateInfo = (index: number) => Array
   .from(
@@ -26,12 +45,46 @@ const indexToCalulateInfo = (index: number) => Array
   )
   .map((v) => Number(v));
 
-const validate = (values:number[]) => {
-  for (let i = 0; i < calculateFunctions.length ** 3; i += 1) {
-    const calculateIndices = indexToCalulateInfo(i);
-    const result = calculate(values, calculateIndices);
-    if (result === 24) return calculateIndices;
+// hacked
+const getPossibles = () => {
+  const source = [0, 1, 2, 3];
+  const possibles: number[][] = [];
+  for (let i = 0; i < 4; i += 1) {
+    for (let i2 = 0; i2 < 3; i2 += 1) {
+      for (let i3 = 0; i3 < 2; i3 += 1) {
+        const possible: number[] = [];
+        [i, i2, i3, 0].reduce((prev, index) => {
+          const [a] = prev.splice(index, 1);
+          possible.push(a);
+          return [...prev];
+        }, [...source]);
+        possibles.push(possible);
+      }
+    }
   }
+  return possibles;
+};
+
+const validate = (values: number[]) => {
+  const possibles = getPossibles();
+  for (let combo = 0; combo < possibles.length - 1; combo += 1) {
+    const reordered = possibles[combo].map((i) => values[i]);
+    for (let pivot = 0; pivot < values.length - 1; pivot += 1) {
+      for (
+        let i = 0;
+        i < calculateFunctions.length ** (reordered.length - 1);
+        i += 1
+      ) {
+        const calculateIndices = indexToCalulateInfo(i);
+        const orderTransformed = reordered
+          .slice(pivot)
+          .concat(reordered.slice(0, pivot));
+        const result = calculate(orderTransformed, calculateIndices);
+        if (result === 24) return [calculateIndices, orderTransformed];
+      }
+    }
+  }
+
   return false;
 };
 
@@ -59,7 +112,7 @@ const App = () => {
     const values = Array.from(value).map((v) => Number(v));
     const result = validate(values);
     if (result) {
-      setOutput(`true, 因為 ${composeProcessString(values, result)} = 24`);
+      setOutput(`true, 因為 ${composeProcessString(result[1], result[0])} = 24`);
     } else {
       setOutput('false');
     }
@@ -72,7 +125,7 @@ const App = () => {
   return (
     <div className='App'>
       <body>
-        <div>
+        <div className='text-3xl'>
           <input
             className='outline'
             type='number'
@@ -83,7 +136,7 @@ const App = () => {
             verify
           </button>
         </div>
-        <div>
+        <div className='text-3xl'>
           {output}
         </div>
       </body>
