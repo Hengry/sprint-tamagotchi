62540eb13e968346ca90dd30a249c640c55f2f1f
Hengry <hengry1213@gmail.com>
2024-02-19 00:23:27 +0800

    pwa & icons

--- next.config.js
+++ next.config.js
@@ -3,9 +3,19 @@ const withTwin = require('./withTwin.js');
 /** @type {import('next').NextConfig} */
 const nextConfig = withTwin({
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
+  },
   experimental: {
     serverActions: true,
   },
 });
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
