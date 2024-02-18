230c903fc5ec1fa22a7c86e88aa146ebb0148764
Hengry <hengry1213@gmail.com>
2024-02-18 18:25:18 +0800

    extract tag names

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import { twJoin } from 'tailwind-merge';
 import { collection, addDoc } from 'firebase/firestore';
 import { Icon } from '@iconify/react';
 import { db } from './_vender/firebase';
+import tagNames from './tags.data';
 
 import Auth from './Auth';
 import { useCallback } from 'react';
 
 const Title = tw.div`p-8`;
-const Button = tw.button`bg-gray-800 rounded whitespace-pre-wrap p-2`;
 
 interface Tag {
   deviceTime: number;
@@ -27,6 +27,7 @@ function TagButton<T extends string>({ onClick, value }: TagButtonProps<T>) {
   const handleClick = useCallback(() => {
     onClick(value);
   }, [onClick, value]);
+
   return (
     <button
       className='bg-gray-800 rounded whitespace-pre-wrap p-2'
@@ -37,30 +38,11 @@ function TagButton<T extends string>({ onClick, value }: TagButtonProps<T>) {
   );
 }
 
-const tagNames = [
-  'Food\n10',
-  'Food\n100',
-  'Food\n1000',
-  'Desert\n10',
-  'Desert\n100',
-  'Desert\n1000',
-  'Alcohol\n10',
-  'Alcohol\n100',
-  'Alcohol\n1000',
-  'Med\n10',
-  'Med\n100',
-  'Med\n1000',
-  'Game\n10',
-  'Game\n100',
-  'Game\n1000',
-  'Ticket\n10',
-  'Ticket\n100',
-  'Ticket\n1000',
-];
-
 export default function Home() {
   const handleClick = async (value: string) => {
-    const tagsCollection = collection(db, 'tags');
+    const collectionName =
+      process.env.NODE_ENV === 'production' ? 'tags' : 'devTags';
+    const tagsCollection = collection(db, collectionName);
     const newTag: Tag = {
       deviceTime: Date.now(),
       value,
@@ -70,7 +52,6 @@ export default function Home() {
   return (
     <main>
       <Auth />
-      <Title>Starter from Hengry</Title>
       <div className='grid grid-cols-3 gap-2 p-2'>
         {tagNames.map((name) => (
           <TagButton key={name} onClick={handleClick} value={name} />
